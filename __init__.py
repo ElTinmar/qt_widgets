92642a4604ba71dba4d707eea6feba30446106b9
ElTinmar <martin.privat@hotmail.fr>
2023-10-27 10:36:05 +0200

    adding labeled ComboBox

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
 __all__ = ["labeled_doublespinbox", "labeled_editline_openfile", "labeled_slider_spinbox", 
-           "labeled_spinbox", "ndarray_to_qpixmap"]
+           "labeled_spinbox", "labeled_combobox", "ndarray_to_qpixmap"]
 
 from .labeled_doublespinbox import *
 from .labeled_editline_openfile import *
 from .labeled_slider_spinbox import *
 from .labeled_spinbox import *
+from .labeled_combobox import *
 from .ndarray_to_qpixmap import *
