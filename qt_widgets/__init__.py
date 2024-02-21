1360baec46aa5cf89fe8416f2e712c481b222b47
ElTinmar <martin.privat@hotmail.fr>
2024-02-21 13:28:36 +0100

    update

--- qt_widgets/__init__.py
+++ qt_widgets/__init__.py
@@ -1,10 +1,12 @@
 __all__ = ["labeled_doublespinbox", "labeled_editline_openfile", "labeled_editline_savefile",
-           "labeled_slider_spinbox", "labeled_spinbox", "labeled_combobox", "ndarray_to_qpixmap"]
+           "labeled_slider_spinbox", "labeled_slider_doublespinbox", "labeled_spinbox", 
+           "labeled_combobox", "ndarray_to_qpixmap"]
 
+from .labeled_spinbox import *
 from .labeled_doublespinbox import *
 from .labeled_editline_openfile import *
 from .labeled_editline_savefile import *
 from .labeled_slider_spinbox import *
-from .labeled_spinbox import *
+from .labeled_slider_doublespinbox import *
 from .labeled_combobox import *
 from .ndarray_to_qpixmap import *
