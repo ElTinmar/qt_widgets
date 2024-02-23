f84ad5fb346a662a67c9843ce4f533206b4e2ec9
ElTinmar <martin.privat@hotmail.fr>
2024-02-23 13:40:02 +0100

    singleton dimension

--- qt_widgets/ndarray_to_qpixmap.py
+++ qt_widgets/ndarray_to_qpixmap.py
@@ -3,6 +3,12 @@ from numpy.typing import NDArray
 import numpy as np
 
 def NDarray_to_QPixmap(img: NDArray) -> QPixmap:
+    
+    # in case we have a singleton dimension
+    if (len(img.shape) == 3) and img.shape[2] == 1:
+        img = np.dstack((img[:,:,0],img[:,:,0],img[:,:,0]))
+
+    # in case we have grayscale
     if len(img.shape) == 2:
         img = np.dstack((img,img,img))
     
