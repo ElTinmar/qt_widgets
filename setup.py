ae77144b97d93ec21b4afcf7413f9568936b0bac
ElTinmar <martin.privat@hotmail.fr>
2025-02-10 18:12:39 +0100

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(
     name='qt_widgets',
     author='Martin Privat',
-    version='0.3.16',
+    version='0.3.17',
     packages=['qt_widgets','qt_widgets.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='custom qt widgets and tools',
     long_description=open('README.md').read(),
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         "numpy",
