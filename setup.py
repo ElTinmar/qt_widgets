5290725ed0301e3fdd5cdd22b446430de792306f
ElTinmar <martin.privat@hotmail.fr>
2025-02-10 18:14:59 +0100

    fix

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup, find_packages
 setup(
     name='qt_widgets',
     author='Martin Privat',
-    version='0.3.17',
+    version='0.3.18',
     packages=['qt_widgets','qt_widgets.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='custom qt widgets and tools',
     long_description=open('README.md').read(),
-    packages=find_packages(),
     include_package_data=True,
+    package_data={"qt_widgets": ["resources/*"]},
     install_requires=[
         "numpy",
         "PyQt5",
