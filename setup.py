86a1b4931a5b9503d2714d2c9bd724d64aec9892
ElTinmar <martin.privat@hotmail.fr>
2025-02-10 18:09:27 +0100

    include package data

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from distutils.core import setup
 setup(
     name='qt_widgets',
     author='Martin Privat',
-    version='0.3.15',
+    version='0.3.16',
     packages=['qt_widgets','qt_widgets.tests'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     description='custom qt widgets and tools',
     long_description=open('README.md').read(),
+    include_package_data=True,
     install_requires=[
         "numpy",
         "PyQt5",
