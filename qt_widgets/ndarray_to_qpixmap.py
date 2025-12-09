a41859147c693cac09c9e42ab5b0054c2b5dffb0
ElTinmar <martin.privat@hotmail.fr>
2025-12-09 10:52:46 +0100

    make sure uint8 in NDArray_to_QPixmap

--- qt_widgets/ndarray_to_qpixmap.py
+++ qt_widgets/ndarray_to_qpixmap.py
@@ -2,8 +2,15 @@ from PyQt5.QtGui import QPixmap, QImage
 from numpy.typing import NDArray
 import numpy as np
 
+def float_to_uint8(img: np.ndarray) -> np.ndarray:
+    if img.dtype == np.float32 or img.dtype == np.float64:
+        img = img * 255.0
+        img = img.clip(0, 255).astype(np.uint8)
+    return img
+
 def NDarray_to_QPixmap(img: NDArray, format = QImage.Format_BGR888) -> QPixmap:
     # note opencv uses BGR format by default
+    img = float_to_uint8(img)
     
     # in case we have a singleton dimension
     if (len(img.shape) == 3) and img.shape[2] == 1:
